47d3467a07bc0237cab70ff88ffe087d1675f5ec
Marty Schlegel <martin.schlegel.1838@outlook.com>
2020-02-03 12:40:49 -0600

    Add custom cell interpretation example.

---
+++ examples/drinks/list-cell-interpreter.js
@@ -0,0 +1,24 @@
+
+
+function ListCellInterpreter() {
+	'use strict';
+
+}
+
+ListCellInterpreter.prototype.populateCellValues = function (cell, values) {
+	'use strict';
+
+	var listElements, i;
+	
+	listElements = cell.getElementsByTagName('li');
+	if (!listElements.length) {
+		return true;
+	}
+	
+	for (i = 0; i < listElements.length; ++i) {
+		values.add(listElements[i].textContent);
+	}
+	
+	// Not strictly necessary; permissable to not have a return value. (No return statement implies a return value of undefined.)
+	return false;
+};
